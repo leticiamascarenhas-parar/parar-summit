bad31bf0bdde5d1e2a291803598d129298e7dd07
leticiamascarenhas-parar <leeticia.mascarenhas@parar.com.br>
2026-07-17 18:39:56 -0300

    alterações trilhas

--- src/data/speakers.ts
+++ src/data/speakers.ts
@@ -4,14 +4,14 @@ export const speakers = [
     cargo: "Palestrante e ex-jogador de futebol",
     foto: "/images/speakers/neto-zampier.jpg",
     descricao: "Palestrante e ex-jogador de futebol.",
-    palestra: "Sobrevivendo com segurança",
+    palestra: "Sobreviver",
     detalhes: "Sobrevivente do trágico acidente aéreo da Chapecoense em 2016, Neto Zampier transformou sua experiência extrema em propósito, tornando-se uma grande referência em resiliência e prevenção de riscos. Nesta palestra, ele compartilha lições valiosas sobre como a cultura da segurança e o respeito aos protocolos salvam vidas, dentro e fora das estradas."
   },
 
   {
     nome: "Rogerio Nersissian",
     cargo: "Especialista em Cultura de Segurança, palestrante e embaixador do Instituto PARAR",
-    foto: "/images/speakers/rogerio-nersissian.jpg",
+    foto: "/images/speakers/rogerio-nerssissian.jpeg",
     descricao: "Especialista em Cultura de Segurança, palestrante e embaixador do Instituto PARAR.",
     palestra: "Falhar com Segurança",
     detalhes: "Erros acontecem, mas as consequências deles podem ser desenhadas. o palestrante e embaixador do Instituto PARAR, Rogério Nersissian, desconstrói a cultura da culpa para ensinar como líderes de frotas podem implementar sistemas resilientes. Uma imersão prática sobre como mapear vulnerabilidades e blindar sua equipe contra incidentes graves."
@@ -23,16 +23,16 @@ export const speakers = [
     foto: "/images/speakers/william-magalhaes.jpeg",
     descricao: "Especialista em Manutenção.",
     palestra: "Gestão de ativos para uma gestão de frotas de alto nível",
-    detalhes: "O renomado especialista William Magalhães traz práticas essenciais de monitoramento e manutenção que minimizam custos operacionais e protegem o capital da empresa. Uma palestra indispensável para gestores que buscam extrair o valor máximo de cada recurso."
+    detalhes: "O especialista William Magalhães traz práticas essenciais de monitoramento e manutenção que minimizam custos operacionais e protegem o capital. Uma palestra indispensável para gestores que buscam extrair o valor máximo de cada recurso."
   },
 
   {
     nome: "Alejandro Furas",
     cargo: "Secretário Geral do Latin NCAP e Global NCAP",
-    foto: "/images/speakers/alejandro-furas.png",
+    foto: "/images/speakers/alejandro-furas.jpeg",
     descricao: "Secretário Geral do Latin NCAP e Global NCAP.",
     palestra: "Segurança Automotiva: o Impacto do Latin NCAP nas Frotas",
-    detalhes: "Ter uma frota segura começa muito antes do veículo ir para a rua. O secretário-geral do Latin NCAP, Alejandro Furas, analisa o panorama da segurança automotiva e o papel das empresas como forças de mudança no setor. Uma palestra fundamental para gestores que desejam alinhar eficiência operacional com o compromisso inegociável de salvar vidas."
+    detalhes: "Ter uma frota segura começa muito antes do veículo ir para a rua. Alejandro Furas analisa o panorama da segurança automotiva e o papel das empresas como forças de mudança no setor. Uma palestra fundamental para gestores que desejam alinhar eficiência com o compromisso inegociável de salvar vidas."
   }
 
 ];
--- src/data/trilhas.ts
+++ src/data/trilhas.ts
@@ -1,4 +1,21 @@
 export const trilhas = [
+    {
+    id: "politica-de-frotas",
+    nome: "Política de Frotas",
+    palestras: [
+      {
+        id: 1,
+        titulo: "Política de frotas na prática ",
+        palestrante: "Carlos Tudisco",
+        cargo: "COO da Golfleet e professor do Instituto PARAR",
+        foto: "/images/trilhas/titulo/carlos-tudisco.jpeg",
+        horario: "a definir",
+        sala: "a definir",
+        descricao: "Descrição da palestra."
+      }
+    ]
+  },
+  
   {
     id: "sustentabilidade",
     nome: "Sustentabilidade",
@@ -8,9 +25,108 @@ export const trilhas = [
         titulo: "O Caminho da Descarbonização: o papel do etanol nas frotas brasileiras",
         palestrante: "Renata Camargo",
         cargo: "Gerente de Sustentabilidade da UNICA",
-        foto: "/images/trilhas/titulo/renata-unica.jpg",
+        foto: "/images/trilhas/titulo/renata-unica.jpeg",
         horario: "21/10 às 13h50",
         sala: "Lab 5",
+      }
+    ]
+  },
+
+  {
+    id: "inovacao",
+    nome: "Inovação",
+    palestras: [
+      {
+        id: 1,
+        titulo: "Gestão de Inovação",
+        palestrante: "Sergio Jabali",
+        cargo: "CTO da Golfleet e professor do Instituto PARAR",
+        foto: "/images/trilhas/titulo/sergio-jabali.jpg",
+        horario: "a definir",
+        sala: "a definir",
+      },
+    ]
+  },
+
+  {
+    id: "gestao-de-dados",
+    nome: "Gestão de Dados",
+    palestras: [
+      {
+        id: 1,
+        titulo: "KPI: Indicadores para Gestão de Frotas",
+        palestrante: "Milad Neto",
+        cargo: "Diretor na KLume e professor do Instituto PARAR",
+        foto: "/images/trilhas/titulo/milad-neto.jpg",
+        horario: "a definir",
+        sala: "a definir",
+      }
+    ]
+  },
+
+  {
+    id: "gestao-de-manutencao",
+    nome: "Gestão de Manutenção",
+    palestras: [
+      {
+        id: 1,
+        titulo: "Gestão de Manutenção para frotas de alta performance",
+        palestrante: "William Magalhães",
+        cargo: "Especialista em Manutenção",
+        foto: "/images/speakers/william-magalhaes.jpeg",
+        horario: "a definir",
+        sala: "a definir",
+      }
+    ]
+  },
+
+  {
+    id: "gestao-de-frotas-publicas",
+    nome: "Gestão de Frotas Públicas",
+    palestras: [
+      {
+        id: 1,
+        titulo: "Política de Frota no Setor Público",
+        palestrante: "Carlos Tudisco",
+        cargo: "COO da Golfleet e professor do Instituto PARAR",
+        foto: "/images/trilhas/titulo/carlos-tudisco.jpeg",
+        horario: "a definir",
+        sala: "a definir",
+      },
+      {
+        id: 2,
+        titulo: "Palestra a definir",
+        palestrante: "Nome do palestrante",
+        cargo: "Cargo do palestrante",
+        foto: "/images/trilhas/placeholder.jpg",
+        horario: "a definir",
+        sala: "a definir",
+      }
+    ]
+  },
+
+
+  {
+    id: "gestao-e-lideranca",
+    nome: "Gestão e Liderança",
+    palestras: [
+      {
+        id: 1,
+        titulo: "Inteligência Emocional na Gestão de Frotas",
+        palestrante: "Eliandro Maurat",
+        cargo: "CEO Instituto Vida Segura",
+        foto: "/images/trilhas/placeholder.jpg",
+        horario: "a definir",
+        sala: "a definir",
+      },
+      {
+        id: 2,
+        titulo: "Palestra a definir",
+        palestrante: "Nome do palestrante",
+        cargo: "Cargo do palestrante",
+        foto: "/images/trilhas/placeholder.jpg",
+        horario: "a definir",
+        sala: "a definir",
       }
     ]
   },
@@ -24,36 +140,10 @@ export const trilhas = [
         titulo: "Palestra a definir",
         palestrante: "Nome do palestrante",
         cargo: "Cargo do palestrante",
-        foto: "/images/trilhas/titulo/renata-unica.jpg",
-        horario: "Horário",
-        sala: "Sala",
-      },
-      {
-        id: 2,
-        titulo: "Palestra a definir",
-        palestrante: "Nome do palestrante",
-        cargo: "Cargo do palestrante",
-        foto: "/images/trilhas/titulo/renata-unica.jpg",
-        horario: "Horário",
-        sala: "Sala",
-      }
-    ]
-  },
-
-  {
-    id: "politica-de-frotas",
-    nome: "Política de Frotas",
-    palestras: [
-      {
-        id: 1,
-        titulo: "Palestra a definir",
-        palestrante: "Nome do palestrante",
-        cargo: "Cargo do palestrante",
         foto: "/images/trilhas/placeholder.jpg",
-        horario: "Horário",
-        sala: "Sala",
-        descricao: "Descrição da palestra."
-      }
+        horario: "a definir",
+        sala: "a definir",
+      },
     ]
   },
 
@@ -67,20 +157,10 @@ export const trilhas = [
         palestrante: "Nome do palestrante",
         cargo: "Cargo do palestrante",
         foto: "/images/trilhas/placeholder.jpg",
-        horario: "Horário",
-        sala: "Sala",
+        horario: "a definir",
+        sala: "a definir",
         descricao: "Descrição da palestra."
       },
-      {
-        id: 2,
-        titulo: "Palestra a definir",
-        palestrante: "Nome do palestrante",
-        cargo: "Cargo do palestrante",
-        foto: "/images/trilhas/placeholder.jpg",
-        horario: "Horário",
-        sala: "Sala",
-        descricao: "Descrição da palestra."
-      }
     ]
   },
 
@@ -94,123 +174,8 @@ export const trilhas = [
         palestrante: "Nome do palestrante",
         cargo: "Cargo do palestrante",
         foto: "/images/trilhas/placeholder.jpg",
-        horario: "Horário",
-        sala: "Sala",
-        descricao: "Descrição da palestra."
-      }
-    ]
-  },
-
-  {
-    id: "gestao-de-dados",
-    nome: "Gestão de Dados",
-    palestras: [
-      {
-        id: 1,
-        titulo: "Palestra a definir",
-        palestrante: "Nome do palestrante",
-        cargo: "Cargo do palestrante",
-        foto: "/images/trilhas/placeholder.jpg",
-        horario: "Horário",
-        sala: "Sala",
-        descricao: "Descrição da palestra."
-      }
-    ]
-  },
-
-  {
-    id: "gestao-de-frotas-publicas",
-    nome: "Gestão de Frotas Públicas",
-    palestras: [
-      {
-        id: 1,
-        titulo: "Palestra a definir",
-        palestrante: "Nome do palestrante",
-        cargo: "Cargo do palestrante",
-        foto: "/images/trilhas/placeholder.jpg",
-        horario: "Horário",
-        sala: "Sala",
-        descricao: "Descrição da palestra."
-      },
-      {
-        id: 2,
-        titulo: "Palestra a definir",
-        palestrante: "Nome do palestrante",
-        cargo: "Cargo do palestrante",
-        foto: "/images/trilhas/placeholder.jpg",
-        horario: "Horário",
-        sala: "Sala",
-        descricao: "Descrição da palestra."
-      }
-    ]
-  },
-
-  {
-    id: "gestao-de-manutencao",
-    nome: "Gestão de Manutenção",
-    palestras: [
-      {
-        id: 1,
-        titulo: "Palestra a definir",
-        palestrante: "Nome do palestrante",
-        cargo: "Cargo do palestrante",
-        foto: "/images/trilhas/placeholder.jpg",
-        horario: "Horário",
-        sala: "Sala",
-        descricao: "Descrição da palestra."
-      }
-    ]
-  },
-
-  {
-    id: "gestao-e-lideranca",
-    nome: "Gestão e Liderança",
-    palestras: [
-      {
-        id: 1,
-        titulo: "Palestra a definir",
-        palestrante: "Nome do palestrante",
-        cargo: "Cargo do palestrante",
-        foto: "/images/trilhas/placeholder.jpg",
-        horario: "Horário",
-        sala: "Sala",
-        descricao: "Descrição da palestra."
-      },
-      {
-        id: 2,
-        titulo: "Palestra a definir",
-        palestrante: "Nome do palestrante",
-        cargo: "Cargo do palestrante",
-        foto: "/images/trilhas/placeholder.jpg",
-        horario: "Horário",
-        sala: "Sala",
-        descricao: "Descrição da palestra."
-      }
-    ]
-  },
-
-  {
-    id: "inovacao",
-    nome: "Inovação",
-    palestras: [
-      {
-        id: 1,
-        titulo: "Palestra a definir",
-        palestrante: "Nome do palestrante",
-        cargo: "Cargo do palestrante",
-        foto: "/images/trilhas/placeholder.jpg",
-        horario: "Horário",
-        sala: "Sala",
-        descricao: "Descrição da palestra."
-      },
-      {
-        id: 2,
-        titulo: "Palestra a definir",
-        palestrante: "Nome do palestrante",
-        cargo: "Cargo do palestrante",
-        foto: "/images/trilhas/placeholder.jpg",
-        horario: "Horário",
-        sala: "Sala",
+        horario: "a definir",
+        sala: "a definir",
         descricao: "Descrição da palestra."
       }
     ]
